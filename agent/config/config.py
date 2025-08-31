66eb04929499ae3b68bde262c32d2f4fb3d52e22
IgorKoricanac <igor.koricanac@gmail.com>
2025-08-31 14:33:11 +0200

    Change agent backend_url

--- agent/config/config.py
+++ agent/config/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     def __init__(self):
-        # Fixed backend URL for security - users cannot modify this
-        self.backend_url = 'http://kure-backend:8000'
+        # Read backend URL from environment variable, with fallback to correct service name
+        self.backend_url = os.getenv('BACKEND_URL', 'http://kure-monitor-backend:8000')
         self.check_interval = int(os.getenv('KURE_CHECK_INTERVAL', '5'))  # seconds
         self.log_level = os.getenv('KURE_LOG_LEVEL', 'INFO')
